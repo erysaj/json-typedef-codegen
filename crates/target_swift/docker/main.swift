66e8db3b4bea151c90bc054106f0551f2923bca0
Eugene Rysaj <eugene.rysaj@gmail.com>
2021-12-19 07:38:23 +0200

    Setup docker e2e tests

--- crates/target_swift/docker/main.swift
+++ crates/target_swift/docker/main.swift
@@ -1,3 +1,8 @@
+import Foundation
+
 while let line = readLine() {
-    print(line)
+    let decoder = JSONDecoder()
+    let payload = try decoder.decode(MAIN.self, from: line.data(using: .utf8)!)
+    let encoder = JSONEncoder()
+    try print(String(data: encoder.encode(payload), encoding: .utf8)!)
 }
